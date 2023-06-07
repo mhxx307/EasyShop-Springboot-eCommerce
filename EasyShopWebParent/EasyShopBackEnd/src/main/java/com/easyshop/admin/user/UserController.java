0e6af4984ac268f66152fe41f8b9c5e62e49bb9d
mhxx307 <minhquan.lavo@gmail.com>
2023-06-08 01:57:20 +0700

    Code new user function

--- EasyShopWebParent/EasyShopBackEnd/src/main/java/com/easyshop/admin/user/UserController.java
+++ EasyShopWebParent/EasyShopBackEnd/src/main/java/com/easyshop/admin/user/UserController.java
@@ -6,9 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.easyshop.common.entity.Role;
 import com.easyshop.common.entity.User;
+
+/**
+ * 
+ * @author La Vo Minh Quan
+ * 
+ * [[@]]: duong dan , path
+ * [[$]]: bien, variable
+ *
+ */
 
 @Controller
 @RequestMapping("/users")
@@ -22,5 +34,23 @@ public class UserController {
 		model.addAttribute("listUsers", listUsers);
 
 		return "users";
+	}
+	
+	@GetMapping("/new")
+	public String newUser(Model model) {
+		User user = new User();
+		List<Role> listRoles = userService.listRoles();
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+		return "user_form";
+	}
+	
+	@PostMapping("/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		userService.save(user);
+		
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		return "redirect:/users";
 	}
 }
--- EasyShopWebParent/EasyShopBackEnd/src/main/java/com/easyshop/admin/user/UserService.java
+++ EasyShopWebParent/EasyShopBackEnd/src/main/java/com/easyshop/admin/user/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.easyshop.common.entity.Role;
 import com.easyshop.common.entity.User;
 
 @Service
@@ -12,9 +13,18 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private RoleRepository roleRepository;
+	
 	public List<User> listAll() {
 		return userRepository.findAll();
 	} 
 	
+	public List<Role> listRoles() {
+		return roleRepository.findAll();
+	}
 	
+	public void save(User user) {
+		userRepository.save(user);
+	}
 }
