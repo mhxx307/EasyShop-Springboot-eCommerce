a2d15dcfee891fb368fad364b424cfd59d57e9e0
mhxx307 <minhquan.lavo@gmail.com>
2023-06-13 12:20:32 +0700

    Code user login page

--- EasyShopWebParent/EasyShopBackEnd/src/main/java/com/easyshop/admin/MainController.java
+++ EasyShopWebParent/EasyShopBackEnd/src/main/java/com/easyshop/admin/MainController.java
@@ -9,4 +9,9 @@ public class MainController {
 	public String viewHomePage() {
 		return "index";
 	}
+	
+	@GetMapping("/login")
+	public String viewLoginPage() {
+		return "login";
+	}
 }
--- EasyShopWebParent/EasyShopBackEnd/src/main/java/com/easyshop/admin/config/SpringSecurityConfig.java
+++ EasyShopWebParent/EasyShopBackEnd/src/main/java/com/easyshop/admin/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,13 +13,38 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SpringSecurityConfig {
 //	create spring bean and manage by spring ioc container to DI in any place not using keyword "new" anymore
+	
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//		return new ShopmeUserDetailsService();
+//	}
+	
 	@Bean
 	public PasswordEncoder PasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> {
+			try {
+				auth
+			    .anyRequest().authenticated()
+			    .and()
+			    .formLogin()
+			        .loginPage("/login")
+			        .permitAll();
+			} catch (Exception e) {
+				System.out.println(e);
+				e.printStackTrace();
+			}
+		});
+
 		return http.build();
+	}
+	
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+	    return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
 	}
 }
