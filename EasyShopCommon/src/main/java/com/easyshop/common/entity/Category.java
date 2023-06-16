78fb12677fda464f52680a0a3ae76c8305ef7f09
mhxx307 <minhquan.lavo@gmail.com>
2023-06-16 18:15:12 +0700

    Code category entity and category repository

---
+++ EasyShopCommon/src/main/java/com/easyshop/common/entity/Category.java
@@ -0,0 +1,47 @@
+package com.easyshop.common.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "categories")
+public class Category {
+	@Id
+	private Integer id;
+	
+	@Column(length = 128, nullable = false, unique = true)
+	private String name;
+	
+	@Column(length = 64, nullable = false, unique = true)
+	private String alias;
+	
+	@Column(length = 128, nullable = false)
+	private String image;
+	
+	private boolean enabled;
+	
+	@OneToOne
+	@JoinColumn(name = "parent_id")
+	private Category parent;
+	
+	@OneToMany(mappedBy = "parent")
+	private Set<Category> children = new HashSet<>();
+}
---
+++ EasyShopWebParent/EasyShopBackEnd/src/main/java/com/easyshop/admin/category/CategoryRepository.java
@@ -0,0 +1,36 @@
+package com.easyshop.admin.category;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.easyshop.common.entity.Category;
+import com.easyshop.common.entity.User;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<User, Integer> {
+	@Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
+	public List<Category> findRootCategories(Sort sort);
+
+	@Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
+	public Page<Category> findRootCategories(Pageable pageable);
+	
+	@Query("SELECT c FROM Category c WHERE c.name LIKE %?1%")
+	public Page<Category> search(String keyword, Pageable pageable);
+	
+	public Long countById(Integer id);
+	
+	public Category findByName(String name);
+	
+	public Category findByAlias(String alias);
+	
+	@Query("UPDATE Category c SET c.enabled = ?2 WHERE c.id = ?1")
+	@Modifying
+	public void updateEnabledStatus(Integer id, boolean enabled);	
+}
